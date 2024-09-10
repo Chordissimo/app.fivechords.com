205a1f25e05fd4cb5f29d3672e2e534d423376f7
antm677 <anton.andreyeff@gmail.com>
2024-09-10 15:34:02 +0300

    Update ssl_setup.sh

--- src/scripts/ssl_setup.sh
+++ src/scripts/ssl_setup.sh
@@ -1,5 +1,18 @@
-echo "Usage: sh ssl_setup.sh <env>"
-echo "env - can be production or staging"
-echo
+if [[ $1 == "" ]]; then
+  echo "Error: domain name not found.
+  echo "Usage: sh ssl_setup.sh <domain_name> <path_to_yaml>"
+  echo "<domain_name> - e.g. get.airchords.app"
+  echo "<path_to_yaml> - config/certbot/docker-compose.certbot.yaml"
+  echo
+fi
+
+if [[ $2 == "" ]]; then
+  echo "Error: docker compose yaml not found.
+  echo "Usage: sh ssl_setup.sh <domain_name> <path_to_yaml>"
+  echo "<domain_name> - e.g. get.airchords.app"
+  echo "<path_to_yaml> - config/certbot/docker-compose.certbot.yaml"
+  echo
+fi
+
 sudo ENV=$1 docker compose -f /home/ci_user/app/config/certbot/docker-compose.certbot-renew.yml run --rm certbot certonly --dry-run --webroot --webroot-path /var/www/certbot/ -d $1.aichords.pro
 (sudo crontab -l ; echo "@monthly sh /home/ci_user/app/scripts/ssl_renew.sh ${1}") | sort - | uniq - | sudo crontab -
