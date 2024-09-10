093ec82ea5a9737fee0d617b84eb199bc20a5250
antm677 <anton.andreyeff@gmail.com>
2024-09-10 15:48:01 +0300

    Update ssl_setup.sh

--- src/scripts/ssl_setup.sh
+++ src/scripts/ssl_setup.sh
@@ -1,5 +1,5 @@
 if [[ $1 == "" ]]; then
-  echo "Error: domain name not found.
+  echo "Error: domain name not found."
   echo "Usage: sh ssl_setup.sh <domain_name> <path_to_yaml>"
   echo "<domain_name> - e.g. get.airchords.app"
   echo "<path_to_yaml> - config/certbot/docker-compose.certbot.yaml"
@@ -7,12 +7,12 @@ if [[ $1 == "" ]]; then
 fi
 
 if [[ $2 == "" ]]; then
-  echo "Error: docker compose yaml not found.
+  echo "Error: docker compose yaml not found."
   echo "Usage: sh ssl_setup.sh <domain_name> <path_to_yaml>"
   echo "<domain_name> - e.g. get.airchords.app"
   echo "<path_to_yaml> - config/certbot/docker-compose.certbot.yaml"
   echo
 fi
 
-sudo ENV=$1 docker compose -f /home/ci_user/app/config/certbot/docker-compose.certbot-renew.yml run --rm certbot certonly --dry-run --webroot --webroot-path /var/www/certbot/ -d $1.aichords.pro
-(sudo crontab -l ; echo "@monthly sh /home/ci_user/app/scripts/ssl_renew.sh ${1}") | sort - | uniq - | sudo crontab -
+sudo ENV=$1 docker compose -f $2 run --rm certbot certonly --dry-run --webroot --webroot-path /var/www/certbot/ -d $1
+#(sudo crontab -l ; echo "@monthly sh /home/ci_user/app/scripts/ssl_renew.sh ${1}") | sort - | uniq - | sudo crontab -
