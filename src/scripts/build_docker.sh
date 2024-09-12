152bd381dd51f2fa02336e6b5205e1118920ee49
antm677 <anton.andreyeff@gmail.com>
2024-09-12 13:22:32 +0300

    Update build_docker.sh

--- src/scripts/build_docker.sh
+++ src/scripts/build_docker.sh
@@ -8,24 +8,24 @@ sudo ln -sf /home/ci_user/.auth ./.auth
 sudo ln -sf /home/ci_user/.certbot ./.certbot
 
 if [[ ${SRV} == *"nginx"* ]]; then
-  sudo ENV=${ENV} docker compose build nginx --no-cache
-  sudo ENV=${ENV} docker compose up -d nginx
+  sudo DOMAIN=${DOMAIN} docker compose build nginx --no-cache
+  sudo DOMAIN=${DOMAIN} docker compose up -d nginx
 fi
 
 if [[ ${SRV} == *"app"* ]]; then
-  sudo ENV=${ENV} docker compose build recognizer retriever --no-cache
-  sudo ENV=${ENV} docker compose up -d recognizer retriever
+  sudo DOMAIN=${DOMAIN} docker compose build recognizer retriever --no-cache
+  sudo DOMAIN=${DOMAIN} docker compose up -d recognizer retriever
 fi
 
 if [[ ${SRV} == *"mongo"* ]]; then
-  sudo ENV=${ENV} docker compose build mongo mongo-express --no-cache
-  sudo ENV=${ENV} docker compose up -d mongo mongo-express
+  sudo DOMAIN=${DOMAIN} docker compose build mongo mongo-express --no-cache
+  sudo DOMAIN=${DOMAIN} docker compose up -d mongo mongo-express
 fi
 
 if [[ ${SRV} == ",," ]]; then
   sudo docker build --no-cache -t app_base -f ./Dockerfile.base .
-  sudo ENV=${ENV} docker compose build --no-cache
-  sudo ENV=${ENV} docker compose up -d
+  sudo DOMAIN=${DOMAIN} docker compose build --no-cache
+  sudo DOMAIN=${DOMAIN} docker compose up -d
 fi
 
 sudo docker system prune -a -f --filter label!="app_base=dont_delete" --volumes
