e690c09fda4ade00095aa309b7f91d29b02a30ab
antm677 <anton.andreyeff@gmail.com>
2024-09-12 15:14:40 +0300

    Update start_portainer.sh

--- src/scripts/start_portainer.sh
+++ src/scripts/start_portainer.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-if [[ $1 == "" ]]; then
+if [[ $1 == "" || $2 == "" ]]; then
   echo
-  echo "Error: path to SSL cert is was not provided."
+  echo "Error: path to Certbot `conf` directory and/or domain were not provided."
   echo
-  echo "Usage: sh ssl_setup.sh <path_to_ssl>"
-  echo "<path_to_ssl> - /home/ci_user/.certbot/conf/live/app.fivchords.com"
+  echo "Usage: sh ssl_setup.sh <domain_name> <path_to_certbot_live> with no trailing slash `/`"
+  echo "<domain_name> - e.g. app.fivechords.com"
+  echo "<path_to_certbot_live> - /home/ci_user/.certbot/conf"
   echo
   exit 1
 fi
+
+echo $2/$1
 
 sudo docker run -d -p 8001:8001 -p 9443:9443 \
         --name portainer \
         --restart=always \
         -v /var/run/docker.sock:/var/run/docker.sock \
         -v portainer_data:/data \
-        -v /home/ci_user/.certbot/conf/live/production.aichords.pro:/certs/live/production.aichords.pro:ro \
-        -v /home/ci_user/.certbot/conf/archive/production.aichords.pro:/certs/archive/production.aichords.pro:ro \
+        -v $2/live/$1:/certs/live/$1:ro \
+        -v $2/archive/$1:/certs/archive/$1:ro \
         portainer/portainer-ce:latest \
-        --sslcert /certs/live/production.aichords.pro/fullchain.pem \
-        --sslkey /certs/live/production.aichords.pro/privkey.pem \
+        --sslcert /certs/live/$1/fullchain.pem \
+        --sslkey /certs/live/$1/privkey.pem \
