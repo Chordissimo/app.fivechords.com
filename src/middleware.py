61722c88991e2b50f975cdb64ec6eecfc00adec2
antm677 <anton.andreyeff@gmail.com>
2024-09-13 17:50:57 +0300

    Update middleware.py

--- src/middleware.py
+++ src/middleware.py
@@ -31,6 +31,7 @@ async def validate_user_middleware(
     ref = request.headers.get("Referer", None)
 
     if not token and ref.startswith("https://app.fivechords.com"):
+        request.state.user_id = "test"
         return await call_next(request)
 
     user_id = None
@@ -39,38 +40,41 @@ async def validate_user_middleware(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="")
         
-    if token and token.startswith("Basic"):
+    # if token and token.startswith("Basic"):
+    #     id_token = token.split(" ").pop()
+    #     decoded_credentials = base64.b64decode(id_token).decode("utf-8")
+    #     username, password = decoded_credentials.split(":")
+    #     assert username == basic_login
+    #     hashed_input_password = crypt.crypt(password, basic_password)
+    #     assert hashed_input_password == basic_password
+    #     user_id = "test"
+    # else:
+    
+    try:
         id_token = token.split(" ").pop()
-        decoded_credentials = base64.b64decode(id_token).decode("utf-8")
-        username, password = decoded_credentials.split(":")
-        assert username == basic_login
-        hashed_input_password = crypt.crypt(password, basic_password)
-        assert hashed_input_password == basic_password
-        user_id = "test"
-    else:
-        try:
-            id_token = token.split(" ").pop()
-            user = get_user_by_token(database=database, token=id_token)
-            if user is None:
-                decoded_token = auth.verify_id_token(id_token)
-                uid = decoded_token.get("uid")
-                exp = decoded_token.get("exp")
-                update_users_token(
-                    database=database,
-                    user_id=uid,
-                    token=id_token,
-                    exp=exp
-                )
-                user_id = uid
-            else:
-                user_id = user["user_id"]
+        user = get_user_by_token(database=database, token=id_token)
+        if user is None:
+            decoded_token = auth.verify_id_token(id_token)
+            uid = decoded_token.get("uid")
+            exp = decoded_token.get("exp")
+            update_users_token(
+                database=database,
+                user_id=uid,
+                token=id_token,
+                exp=exp
+            )
+            user_id = uid
+        else:
+            user_id = user["user_id"]
 
-        except ExpiredIdTokenError:
-            traceback.print_exc()
-            raise HTTPException(status_code=403)
-        except Exception as e:
-            traceback.print_exc()
-            raise HTTPException(status_code=500, detail=e.__str__())
+    except ExpiredIdTokenError:
+        traceback.print_exc()
+        raise HTTPException(status_code=403)
+        
+    except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=e.__str__())
+            
     assert user_id is not None
     request.state.user_id = user_id
     response = await call_next(request)
