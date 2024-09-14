84ba7e7d6ed30917b661658a95656b08d475c909
antm677 <anton.andreyeff@gmail.com>
2024-09-14 19:32:57 +0300

    Update snapshot_download.py

--- src/services/snapshot_download.py
+++ src/services/snapshot_download.py
@@ -1,27 +1,35 @@
 import huggingface_hub
 import logging
 import requests
+from tqdm.auto import tqdm
 
-repo_id = "base"
+_MODELS = {
+     "tiny.en": "Systran/faster-whisper-tiny.en",
+     "tiny": "Systran/faster-whisper-tiny",
+     "base.en": "Systran/faster-whisper-base.en",
+     "base": "Systran/faster-whisper-base",
+     "small.en": "Systran/faster-whisper-small.en",
+     "small": "Systran/faster-whisper-small",
+     "medium.en": "Systran/faster-whisper-medium.en",
+     "medium": "Systran/faster-whisper-medium",
+     "large-v1": "Systran/faster-whisper-large-v1",
+     "large-v2": "Systran/faster-whisper-large-v2",
+     "large-v3": "Systran/faster-whisper-large-v3",
+     "large": "Systran/faster-whisper-large-v3",
+     "distil-large-v2": "Systran/faster-distil-whisper-large-v2",
+     "distil-medium.en": "Systran/faster-distil-whisper-medium.en",
+     "distil-small.en": "Systran/faster-distil-whisper-small.en",
+     "distil-large-v3": "Systran/faster-distil-whisper-large-v3",
+}
 
-# _MODELS = {
-#     "tiny.en": "Systran/faster-whisper-tiny.en",
-#     "tiny": "Systran/faster-whisper-tiny",
-#     "base.en": "Systran/faster-whisper-base.en",
-#     "base": "Systran/faster-whisper-base",
-#     "small.en": "Systran/faster-whisper-small.en",
-#     "small": "Systran/faster-whisper-small",
-#     "medium.en": "Systran/faster-whisper-medium.en",
-#     "medium": "Systran/faster-whisper-medium",
-#     "large-v1": "Systran/faster-whisper-large-v1",
-#     "large-v2": "Systran/faster-whisper-large-v2",
-#     "large-v3": "Systran/faster-whisper-large-v3",
-#     "large": "Systran/faster-whisper-large-v3",
-#     "distil-large-v2": "Systran/faster-distil-whisper-large-v2",
-#     "distil-medium.en": "Systran/faster-distil-whisper-medium.en",
-#     "distil-small.en": "Systran/faster-distil-whisper-small.en",
-#     "distil-large-v3": "Systran/faster-distil-whisper-large-v3",
-# }
+size = "base"
+repo_id = _MODELS.get(size)
+local_dir = "/etc/model_snapshot/" + size
+
+class disabled_tqdm(tqdm):
+    def __init__(self, *args, **kwargs):
+        kwargs["disable"] = True
+        super().__init__(*args, **kwargs)
 
 allow_patterns = [
         "config.json",
@@ -32,7 +40,7 @@ allow_patterns = [
     ]
 
 kwargs = {
-    "local_dir": "data",
+    "local_dir": local_dir,
     "allow_patterns": allow_patterns,
     "tqdm_class": disabled_tqdm,
 }
@@ -43,9 +51,9 @@ except (
     huggingface_hub.utils.HfHubHTTPError,
     requests.exceptions.ConnectionError,
 ) as exception:
-    logger = get_logger()
+    logger = logging.getLogger("faster_whisper")
     logger.warning(
         "An error occured while synchronizing the model %s from the Hugging Face Hub:\n%s",
         repo_id,
         exception,
-    )
+    )        
