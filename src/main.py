b7f2e7ee6508c7a48389fa75947a0aff2dc236d8
antm677 <anton.andreyeff@gmail.com>
2024-09-15 12:00:39 +0300

    [r][app] Update main.py

--- src/main.py
+++ src/main.py
@@ -187,7 +187,7 @@ async def recognize_youtube(
         chord_chunks[-1].end = max(int(len(samples) / 16),
                                    chord_chunks[-1].start)
 
-        model_id = "base" if "/api/recognize/youtube/loader/" not in str(request.url) else "large-v3"
+        model_id = "base" if "/api/recognize/youtube/loader/" not in request.url.path else "large-v3"
         logger.info("Using model: " + model_id)
         text_chunks = SpeechRecognizer.recognize(
             samples,
