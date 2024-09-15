76aaa4c386a9f665fb278c922875b04e25ade273
antm677 <anton.andreyeff@gmail.com>
2024-09-15 09:25:46 +0300

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,6 +1,24 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
+_MODELS = {
+     "tiny.en": "Systran/faster-whisper-tiny.en",
+     "tiny": "Systran/faster-whisper-tiny",
+     "base.en": "Systran/faster-whisper-base.en",
+     "base": "Systran/faster-whisper-base",
+     "small.en": "Systran/faster-whisper-small.en",
+     "small": "Systran/faster-whisper-small",
+     "medium.en": "Systran/faster-whisper-medium.en",
+     "medium": "Systran/faster-whisper-medium",
+     "large-v1": "Systran/faster-whisper-large-v1",
+     "large-v2": "Systran/faster-whisper-large-v2",
+     "large-v3": "Systran/faster-whisper-large-v3",
+     "large": "Systran/faster-whisper-large-v3",
+     "distil-large-v2": "Systran/faster-distil-whisper-large-v2",
+     "distil-medium.en": "Systran/faster-distil-whisper-medium.en",
+     "distil-small.en": "Systran/faster-distil-whisper-small.en",
+     "distil-large-v3": "Systran/faster-distil-whisper-large-v3",
+}
 
 class Chord(BaseModel):
     chord: str
