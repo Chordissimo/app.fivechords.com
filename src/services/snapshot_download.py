1a6e184a1e4fac61285e561eed530e6e372b33f9
antm677 <anton.andreyeff@gmail.com>
2024-09-15 09:41:33 +0300

    Update snapshot_download.py

--- src/services/snapshot_download.py
+++ src/services/snapshot_download.py
@@ -3,6 +3,11 @@ import logging
 import requests
 from tqdm.auto import tqdm
 
+class disabled_tqdm(tqdm):
+    def __init__(self, *args, **kwargs):
+        kwargs["disable"] = True
+        super().__init__(*args, **kwargs)
+         
 _MODELS = {
      "tiny.en": "Systran/faster-whisper-tiny.en",
      "tiny": "Systran/faster-whisper-tiny",
@@ -22,38 +27,33 @@ _MODELS = {
      "distil-large-v3": "Systran/faster-distil-whisper-large-v3",
 }
 
-size = "base"
-repo_id = _MODELS.get(size)
-local_dir = "/etc/model_snapshot/" + size
+model_sizes = ["base","large-v2"]
 
-class disabled_tqdm(tqdm):
-    def __init__(self, *args, **kwargs):
-        kwargs["disable"] = True
-        super().__init__(*args, **kwargs)
-
-allow_patterns = [
-        "config.json",
-        "preprocessor_config.json",
-        "model.bin",
-        "tokenizer.json",
-        "vocabulary.*",
+for size in model_sizes:
+     repo_id = _MODELS.get(size)
+     local_dir = "/etc/model_snapshot/" + size
+     allow_patterns = [
+          "config.json",
+          "preprocessor_config.json",
+          "model.bin",
+          "tokenizer.json",
+          "vocabulary.*",
     ]
-
-kwargs = {
-    "local_dir": local_dir,
-    "allow_patterns": allow_patterns,
-    "tqdm_class": disabled_tqdm,
-}
-
-try:
-    huggingface_hub.snapshot_download(repo_id, **kwargs)
-except (
-    huggingface_hub.utils.HfHubHTTPError,
-    requests.exceptions.ConnectionError,
-) as exception:
-    logger = logging.getLogger("faster_whisper")
-    logger.warning(
-        "An error occured while synchronizing the model %s from the Hugging Face Hub:\n%s",
-        repo_id,
-        exception,
-    )        
+     kwargs = {
+          "local_dir": local_dir,
+          "allow_patterns": allow_patterns,
+          "tqdm_class": disabled_tqdm,
+     }
+     
+     try:
+          huggingface_hub.snapshot_download(repo_id, **kwargs)
+     except (
+          huggingface_hub.utils.HfHubHTTPError,
+          requests.exceptions.ConnectionError,
+     ) as exception:
+          logger = logging.getLogger("faster_whisper")
+          logger.warning(
+               "An error occured while synchronizing the model %s from the Hugging Face Hub:\n%s",
+               repo_id,
+               exception,
+          )
