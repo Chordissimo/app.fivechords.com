4877265838d07fa3567bb4d057ab06daf3623a79
antm677 <anton.andreyeff@gmail.com>
2024-09-21 10:22:34 +0300

    Update start_links_loader.sh

--- src/scripts/start_links_loader.sh
+++ src/scripts/start_links_loader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build --no-cache -t links_loader -f ./Dockerfile.loader . && \
-sudo docker run -it -rm --name links_loader --network src_fivechords links_loader
+sudo docker run -it --rm --name links_loader --network src_fivechords links_loader
