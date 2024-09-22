fb7efdaf079b6e2715a6e29a86a2aa85ecd1d17b
antm677 <anton.andreyeff@gmail.com>
2024-09-22 18:37:24 +0300

    Update links_loader.py

--- src/services/links_loader.py
+++ src/services/links_loader.py
@@ -2,6 +2,8 @@ import requests
 from pytube import Playlist
 from links import _LINKS
 from datetime import datetime, timedelta
+import time
+import random
 
 stdlog = log.getLogger ('stdout')
 recognizer_endpoint = "http://recognizer:8000/api/recognize/youtube/loader/test"
@@ -16,10 +18,12 @@ for p in _LINKS:
     for url in playlist.video_urls[:3]:
         request_body = {'url': url}
         start = datetime.now()
-        response = requests.post(recognizer_endpoint, headers = headers, json = request_body)
+        time.sleep(random.randint(1, 5))
+        # response = requests.post(recognizer_endpoint, headers = headers, json = request_body)
         finish = datetime.now()
         diff = finish - start
         diffs.append(diff)
-        stdout.info("Status code: " + response.status_code + " , commpleted in: ", diff.total_seconds())
+        # stdout.info("Status code: " + response.status_code + " , commpleted in: ", diff.total_seconds())
+        stdout.info("Status code: 200, commpleted in: ", diff.total_seconds())
 
 stdout.info("Done. Average processing time: " + sum(diffs) / len(diffs))
