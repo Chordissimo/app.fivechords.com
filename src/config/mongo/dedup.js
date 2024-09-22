6955c58c970ff6f50307459b8c516097cfebc9bf
antm677 <anton.andreyeff@gmail.com>
2024-09-22 17:09:25 +0300

    Create dedup.js

---
+++ src/config/mongo/dedup.js
@@ -0,0 +1,9 @@
+db = new Mongo().getDB("aichords");
+db.RECOGNITIONS.aggregate([
+  { $sort: { video_id: 1, _id: -1 } }, 
+  { $group: { _id: "$video_id", dups: { $push: "$_id" }, count: { $sum: 1 } } },
+  { $match: { count: { $gt: 1 } } }
+], { allowDiskUse: true }).forEach(function (doc) {
+  doc.dups.shift();
+  db.RECOGNITIONS.remove({ _id: { $in: doc.dups } })
+});
