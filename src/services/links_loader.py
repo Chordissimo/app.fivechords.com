914b7525b507770674302c31caac4e2883f4d1cd
antm677 <anton.andreyeff@gmail.com>
2024-09-22 19:05:55 +0300

    Update links_loader.py

--- src/services/links_loader.py
+++ src/services/links_loader.py
@@ -2,10 +2,11 @@ import requests
 from pytube import Playlist
 from links import _LINKS
 from datetime import datetime, timedelta
-import time
-import random
+import logging
+import sys
 
-stdlog = log.getLogger ('stdout')
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+stdout = logging.getLogger()
 recognizer_endpoint = "http://recognizer:8000/api/recognize/youtube/loader/test"
 headers = {"Referer": "https://app.fivechords.com"}
 
@@ -14,16 +15,17 @@ diffs = []
 for p in _LINKS:
     playlist = Playlist(p)
     stdout.info("Starting playlist: " + p)
-    
-    for url in playlist.video_urls[:3]:
+
+    for url in playlist.video_urls:
+        stdout.info("Processing: " + url)
         request_body = {'url': url}
         start = datetime.now()
-        time.sleep(random.randint(1, 5))
-        # response = requests.post(recognizer_endpoint, headers = headers, json = request_body)
+        response = requests.post(recognizer_endpoint, headers = headers, json = request_body)
         finish = datetime.now()
         diff = finish - start
-        diffs.append(diff)
-        # stdout.info("Status code: " + response.status_code + " , commpleted in: ", diff.total_seconds())
-        stdout.info("Status code: 200, commpleted in: ", diff.total_seconds())
+        diffs.append(diff.total_seconds())
+        stdout.info("Status code: " + str(response.status_code) + " , commpleted in: " + str(diff.total_seconds()))
 
-stdout.info("Done. Average processing time: " + sum(diffs) / len(diffs))
+    stdout.info("Done")
+
+stdout.info("Average processing time: " + str(sum(diffs) / len(diffs)))
