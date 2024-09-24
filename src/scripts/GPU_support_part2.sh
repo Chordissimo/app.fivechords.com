d085bfa757ec98f7dd735e3cf2868f27af73d5c7
antm677 <anton.andreyeff@gmail.com>
2024-09-24 10:57:28 +0300

    Update GPU_support_part2.sh

--- src/scripts/GPU_support_part2.sh
+++ src/scripts/GPU_support_part2.sh
@@ -5,4 +5,8 @@ sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-6
 
+echo 'export PATH="/sbin:/bin:/usr/sbin:/usr/bin:${PATH}:/usr/local/cuda/bin"' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}' >> ~/.bashrc
+source ~/.bashrc
+
 suo reboot
