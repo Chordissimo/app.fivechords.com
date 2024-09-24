595c1261b1166a57c92c350cd57091ae3fcddaf7
antm677 <anton.andreyeff@gmail.com>
2024-09-24 06:01:51 +0300

    Update GPU_support_part1.sh

--- src/scripts/GPU_support_part1.sh
+++ src/scripts/GPU_support_part1.sh
@@ -1,4 +1,5 @@
 # Prrequisites: Ubuntu 22.04
+# https://hostkey.com/documentation/technical/gpu/nvidia_gpu_linux/#installing-nvidia-modules-for-docker
 
 sudo ubuntu-drivers install --gpgpu nvidia:535-server
 sudo apt-get install -y nvidia-driver-535
