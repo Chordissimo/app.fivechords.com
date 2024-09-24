a699654ca2ccba314bb68dea8f46e1e6bf57312a
antm677 <anton.andreyeff@gmail.com>
2024-09-24 15:47:20 +0300

    Update GPU_support_part1.sh

--- src/scripts/GPU_support_part1.sh
+++ src/scripts/GPU_support_part1.sh
@@ -1,6 +1,6 @@
-sudo apt-get remove --purge '^cuda-.*'
-sudo apt-get remove --purge '^libnvidia-.*'
-sudo apt-get remove --purge '^nvidia-.*'
+sudo apt-get remove --purge '^cuda-.*' && \
+sudo apt-get remove --purge '^libnvidia-.*' && \
+sudo apt-get remove --purge '^nvidia-.*' && \
 sudo apt-get autoremove
 
 sudo ubuntu-drivers autoinstall
