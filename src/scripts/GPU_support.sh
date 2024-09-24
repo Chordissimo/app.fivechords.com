0d37003aab042857f3372f325dabfb8bc1b4b0fc
antm677 <anton.andreyeff@gmail.com>
2024-09-24 19:12:17 +0300

    [r] Update and rename GPU_support_part2.sh to GPU_support.sh

---
+++ src/scripts/GPU_support.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+sudo apt-get -y install gcc make
+wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda_12.4.0_550.54.14_linux.run
+sudo sh cuda_12.4.0_550.54.14_linux.run
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sudo apt-get update
+sudo apt-get -y install nvidia-container-toolkit
