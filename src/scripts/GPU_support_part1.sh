6618b2f001846221ca9d45d2ee4361f5c409d387
antm677 <anton.andreyeff@gmail.com>
2024-09-24 10:53:39 +0300

    Update and rename GPU_support_autoinstall.sh to GPU_support_part1.sh

---
+++ src/scripts/GPU_support_part1.sh
@@ -0,0 +1,8 @@
+sudo apt-get remove --purge '^cuda-.*'
+sudo apt-get remove --purge '^libnvidia-.*'
+sudo apt-get remove --purge '^nvidia-.*'
+sudo apt-get autoremove
+
+sudo ubuntu-drivers autoinstall
+
+sudo reboot
