7668e4162f848fa8a9da723e2f5d0eb6cb518688
antm677 <anton.andreyeff@gmail.com>
2024-09-25 20:39:29 +0300

    Update speech_recognizer.py

--- src/services/speech_recognizer.py
+++ src/services/speech_recognizer.py
@@ -6,7 +6,16 @@ from transformers import AutoModelForSpeechSeq2Seq, \
     AutoProcessor, pipeline, Pipeline
 from pytube import CaptionQuery, Caption
 import re
+import logging
+import sys
 
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=_LOGGING_LEVEL,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    stream=sys.stdout
+)
+logger = logging.getLogger()
 
 class SpeechRecognizer:
     __initialized = False
